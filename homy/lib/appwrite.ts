76071eb77442b3739cbfdcc8492283811fb95ca8
alminim <alpriven@gmail.com>
2026-04-30 21:09:36 +0300

    added house creation and other functions

--- homy/lib/appwrite.ts
+++ homy/lib/appwrite.ts
@@ -1,4 +1,4 @@
-import { Client, TablesDB, Account } from "react-native-appwrite";
+import { Client, TablesDB, Account, Teams } from "react-native-appwrite";
 
 const client = new Client();
 client
@@ -8,3 +8,5 @@ client
 
 
 export const account = new Account(client);
+export const team = new Teams(client);
+export const databases = new TablesDB(client);
