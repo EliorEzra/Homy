3439b3298599e64019940310f2bf7f54cc743023
alminim <alpriven@gmail.com>
2026-06-12 16:49:39 +0300

    Added new linter config to catch deprecation warnings

--- homy/eslint.config.js
+++ homy/eslint.config.js
@@ -1,10 +1,25 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const tseslint = require('typescript-eslint');
+const js = require('@eslint/js');
+
 
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
   },
+  {
+    extends: [js.configs.recommended, tseslint.configs.recommendedTypeChecked],
+    files: ["**/*.js", "**/*.jsx", "**/*.tsx", "**/*.ts"],
+    rules: {
+      "@typescript-eslint/no-deprecated": "error"
+    },
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  }
 ]);
